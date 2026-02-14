046584aa8ca7f5c7326877c5ab9b7409c2405c1c
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-02-14 22:27:06 +0200

    Create class for exercise data encapsulation

---
+++ src/flutter/lib/utils/exercise_data.dart
@@ -0,0 +1,11 @@
+class ExerciseData {
+  final String name;
+  final int sets;
+  final int reps;
+
+  const ExerciseData({
+    required this.name,
+    required this.sets,
+    required this.reps
+  });
+}
