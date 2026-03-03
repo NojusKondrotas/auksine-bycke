4b8a464b67e8162b60e47d4edbdc2b1c265e8f6d
arnabe <elynxzz@gmail.com>
2026-03-03 18:28:01 +0200

    Added about us

--- src/flutter/lib/pages/settings_page.dart
+++ src/flutter/lib/pages/settings_page.dart
@@ -92,9 +92,75 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("About Us"), centerTitle: true),
-      body: const Center(
-        child: Text("Auksinė byckė fitness app."),
+      appBar: AppBar(title: const Text("About us"), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Auksinė Byckė",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              "Created by students, seeking an easy and free way to track your fitness journey.",
+              style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+            ),
+            const SizedBox(height: 30),
+
+            _buildPunctualPoint(
+              Icons.flash_on,
+              "Effectiveness",
+              "Fast way to track your progress.",
+            ),
+            _buildPunctualPoint(
+              Icons.storage,
+              "Privacy and security",
+              "All data is stored only on your device.",
+            ),
+            _buildPunctualPoint(
+              Icons.trending_up,
+              "Motivation",
+              "Helps you stay motivated to reach your goals.",
+            ),
+            _buildPunctualPoint(
+              Icons.attach_money,
+              "Free",
+              "Open sourced.",
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _buildPunctualPoint(IconData icon, String title, String description) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.black, size: 30),
+          const SizedBox(width: 15),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(description, style: const TextStyle(color: Colors.grey)),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
