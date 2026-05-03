6d43286f17407b36b872fac92cfb087739eac10e
Matas <samulionis.matas@gmail.com>
2026-05-03 21:04:39 +0300

    feat: Workout days are now colored on the calendar

--- src/flutter/lib/pages/calendar_page.dart
+++ src/flutter/lib/pages/calendar_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:auksine_bycke/database/database_helper.dart';
+import 'package:auksine_bycke/workouts/workout_models.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
@@ -12,8 +14,38 @@ class _CalendarPageState extends State<CalendarPage> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
+  Map<DateTime, WorkoutModel> _workoutByDay = {};
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadWorkouts();
+  }
+
+  Future<void> _loadWorkouts() async {
+    final all = await DatabaseHelper.instance.getAllWorkouts();
+    final completed = all.where((w) => !w.name.startsWith('[ROUTINE]'));
+    setState(() {
+      _workoutByDay = {
+        for (final w in completed) _normalise(w.date): w,
+      };
+      _isLoading = false;
+    });
+  }
+
+  DateTime _normalise(DateTime d) => DateTime(d.year, d.month, d.day);
+
+  bool _hasWorkout(DateTime day) => _workoutByDay.containsKey(_normalise(day));
+
   @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Workout Calendar'),
@@ -70,6 +102,29 @@ class _CalendarPageState extends State<CalendarPage> {
                   fontWeight: FontWeight.bold,
                 ),
                 outsideDaysVisible: false,
+              ),
+              calendarBuilders: CalendarBuilders(
+                defaultBuilder: (context, day, focusedDay) {
+                  if (_hasWorkout(day)) {
+                    return Container(
+                      margin: const EdgeInsets.all(4),
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFFFD700),
+                        shape: BoxShape.circle,
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        '${day.day}',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                      ),
+                    );
+                  }
+                  return null;
+                },
               ),
             ),
           ),
